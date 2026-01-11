899c83d2ddbefd7deca7afec42674e48fd6bf83f
Daniel Allen <dan@aeternagroup.net>
2026-01-11 15:36:45 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "kusto_cluster_principal_assignments" {
+  description = "All kusto_cluster_principal_assignment resources"
+  value       = azurerm_kusto_cluster_principal_assignment.kusto_cluster_principal_assignments
+}
+
